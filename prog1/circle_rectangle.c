40e3b675bb6cca42dc72a8896869e30064a2c334
Prém Ferenc <70635844+ferencprem2@users.noreply.github.com>
2026-02-15 23:12:24 +0100

    Update circle_rectangle.c

--- prog1/circle_rectangle.c
+++ prog1/circle_rectangle.c
@@ -7,7 +7,7 @@ int main() {
 	int rect_a;
 	int rect_b;
 
-	int circ_r;
+	double circ_r;
 
 	printf("Adja meg a téglalap oldalait (egész szám): \n");
 	scanf("%d %d", &rect_a, &rect_b);
@@ -15,8 +15,8 @@ int main() {
 	printf("Terület: %d \n", (rect_a * rect_b)); 
 	printf("Kerület: %d \n", (2 * (rect_a + rect_b)));
 
-	printf("Adja meg a kör sugarát: \n");
-	scanf("%d", &circ_r);
+	printf("Adja meg a kör sugarát (valós szám): \n");
+	scanf("%lf", &circ_r);
 
 	printf("Kör területe: %f \n", (pow(circ_r, 2) * M_PI));
 	printf("Kör kerülete: %f \n", (2 * circ_r * M_PI));
