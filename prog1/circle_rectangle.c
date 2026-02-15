8ffde41a3898839366795b3014434b4f07eee9b8
ferencprem2 <fercsusz0508@gmail.com>
2026-02-15 21:49:24 +0100

    hw done

---
+++ prog1/circle_rectangle.c
@@ -0,0 +1,26 @@
+#define _USE_MATH_DEFINES
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int rect_a;
+	int rect_b;
+
+	int circ_r;
+
+	printf("Adja meg a téglalap oldalait (egész szám): \n");
+	scanf("%d %d", &rect_a, &rect_b);
+
+	printf("Terület: %d \n", (rect_a * rect_b)); 
+	printf("Kerület: %d \n", (2 * (rect_a + rect_b)));
+
+	printf("Adja meg a kör sugarát: \n");
+	scanf("%d", &circ_r);
+
+	printf("Kör területe: %f \n", (pow(circ_r, 2) * M_PI));
+	printf("Kör kerülete: %f \n", (2 * circ_r * M_PI));
+
+
+	return 0;
+}
