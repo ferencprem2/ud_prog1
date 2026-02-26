e0432481642f2e9ab66ac6fb026dab726420a990
ferencprem2 <fercsusz0508@gmail.com>
2026-02-26 20:14:58 +0100

    Renamed branch, deleted first week's folder

---
+++ 1w-circle_rectangle.c
@@ -0,0 +1,28 @@
+#define _USE_MATH_DEFINES
+
+#include <stdio.h>
+#include <math.h>
+
+// ---- TÉGLALAP-KÖR KERÜLET/TERÜLET ----
+
+int main() {
+	int rect_a;
+	int rect_b;
+
+	double circ_r;
+
+	printf("Adja meg a téglalap oldalait (egész szám): \n");
+	scanf("%d %d", &rect_a, &rect_b);
+
+	printf("Terület: %d \n", (rect_a * rect_b)); 
+	printf("Kerület: %d \n", (2 * (rect_a + rect_b)));
+
+	printf("Adja meg a kör sugarát (valós szám): \n");
+	scanf("%lf", &circ_r);
+
+	printf("Kör területe: %f \n", (pow(circ_r, 2) * M_PI));
+	printf("Kör kerülete: %f \n", (2 * circ_r * M_PI));
+
+
+	return 0;
+}
